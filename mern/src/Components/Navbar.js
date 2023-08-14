0b4ed5b56587fb817586eb4592e90660ca9eaa65
Nirmal J13 <nirmalnj2003@gmail.com>
2023-08-14 14:12:56 +0530

    user item change logo

--- mern/src/Components/Navbar.js
+++ mern/src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar()
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link to="#" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">User Management</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">User Item Management</span>
     </Link>
     <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
       <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
