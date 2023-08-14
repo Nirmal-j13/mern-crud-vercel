62dd43a972c5de385a3979085ff0c9318c780dff
Nirmal J13 <nirmalnj2003@gmail.com>
2023-08-14 13:01:42 +0530

    changed cors

--- mern backend/server.js
+++ mern backend/server.js
@@ -6,7 +6,13 @@ const app=express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["mern-crud-vercel.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 
 app.use(express.urlencoded({extended:true}));
 
