48e4cddbec0b995ba29e1eee735fe51ffaecbe23
Nirmal J13 <nirmalnj2003@gmail.com>
2023-09-09 13:18:04 +0530

    changed bookauth homeform

--- mern/src/Components/Homeform.js
+++ mern/src/Components/Homeform.js
@@ -89,7 +89,8 @@ export default function Homeform()
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                    {val.Bookname}
                 </th>
-                <th scope="row" class="box-content h-12 w-32 font-medium dark:text-white">
+                //box-content h-12 w-32 font-medium dark:text-white
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                    {val.Bookauth}
                 </th>
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
